809a69649e5942092cee1c084f5709d4b6b02a92
Glen Smith <glen@bytecode.com.au>
2016-10-20 06:40:40 +1100

    Added test for regexp POST splitter

--- src/app/pocket.service.spec.ts
+++ src/app/pocket.service.spec.ts
@@ -13,4 +13,24 @@ describe('Service: Pocket', () => {
   it('should ...', inject([PocketService], (service: PocketService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  it('should split POST data into a nice map', function () {
+
+    let postData = "one=1&two=2&three=3&four=4";
+    let postMap = {};
+
+    var replacer = function (match: string, key: string, joiner: string, value: string) {
+      postMap[key] = value;
+      return match;
+    }
+
+    postData.replace(
+      new RegExp("([^?=&]+)(=([^&]*))?", "g"), replacer);
+
+    expect(postMap['one']).toBe('1');
+    expect(postMap['two']).toBe('2');  
+
+  });
+
 });
