b3f9c01c3b6fc91f2e6b80adc123361efcdb3a4d
Glen Smith <glen@bytecode.com.au>
2016-10-21 06:42:20 +1100

    Added sample config file for placeholder entries.

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,7 @@
+// This template shows you the fields that will be required
+// in the environment file.
+
+export const environment = {
+  production: false,
+  pocketKey: 'your-pocket-api-key-here'
+};
